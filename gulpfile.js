de35a767135a10b086167c5eb7abe4f055cf0cba
mrmrs <hi@mrmrs.cc>
2014-12-16 02:34:57 -0800

    Remove unnecessary packages. Add another.

--- gulpfile.js
+++ gulpfile.js
@@ -13,11 +13,10 @@ var gulp = require('gulp'),
     pngcrush = require('imagemin-pngcrush'),
     browserSync = require('browser-sync'),
     csslint = require('gulp-csslint'),
-    browserReload = browserSync.reload,
-    rimraf = require('gulp-rimraf'),
-    cp = require('child_process');
+    shell = require('gulp-shell'),
+    browserReload = browserSync.reload;
 
-// Initialize browser-sync which starts a static server also allows for 
+// Initialize browser-sync which starts a static server also allows for
 // browsers to reload on filesave
 gulp.task('browser-sync', function() {
     browserSync.init(null, {
@@ -32,9 +31,12 @@ gulp.task('bs-reload', function () {
     browserSync.reload();
 });
 
+gulp.task('shorthand', shell.task([
+  'jekyll build --watch'
+]))
 
 gulp.task('minify-css', function(){
-gulp.src('./css/nkd.css') // set this to the file(s) you want to minify. 
+gulp.src('./css/nkd.css') // set this to the file(s) you want to minify.
     .pipe(minifyCSS())
     .pipe(size({gzip: false, showFiles: true, title:'minified css'}))
     .pipe(size({gzip: true, showFiles: true, title:'minified css'}))
@@ -87,36 +89,16 @@ gulp.task('pre-process', function(){
           .pipe(size({gzip: false, showFiles: true, title:'after vendor prefixes'}))
           .pipe(size({gzip: true, showFiles: true, title:'after vendor prefixes'}))
           .pipe(gulp.dest('css'))
+          .pipe(minifyCSS())
+          .pipe(size({gzip: false, showFiles: true, title:'minified css'}))
+          .pipe(size({gzip: true, showFiles: true, title:'minified css'}))
+          .pipe(rename('nkd.min.css'))
           .pipe(browserSync.reload({stream:true}));
       }));
 });
 
 gulp.task('production', function(){
     gulp.run('minify-css', 'minify-img', 'minify-svg');
-});
-
-// Remove _site from directory before committing
-gulp.task('clean', function() {
-  return gulp.src('_site/', { read: false })
-    .pipe(rimraf({ force: true }));
-});
-
-// Build the Jekyll Site
-gulp.task('jekyll-build', function (done) {
-    return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
-        .on('close', done);
-});
-
-// Watch & Build the Jekyll Site
-gulp.task('jekyll-watch', function (done) {
-    return cp.spawn('jekyll', ['build', '--watch'], {stdio: 'inherit'})
-        .on('close', done);
-});
-
-// Serve the Jekyll Site
-gulp.task('jekyll-serve', function (done) {
-    return cp.spawn('jekyll', ['serve', '--watch', '--config', '_config.yml,_development_config.yml'], {stdio: 'inherit'})
-        .on('close', done);
 });
 
 /*
@@ -127,10 +109,9 @@ gulp.task('jekyll-serve', function (done) {
  • Reloads browsers when you change html or sass files
 
 */
-gulp.task('default', ['pre-process', 'minify-css', 'bs-reload', 'browser-sync'], function(){
-  gulp.start('pre-process', 'csslint');
-  gulp.watch(['_includes/*', '_layouts/*', '_posts/*', '_resources/*', '_sass/*', '*.html', '_*config.yml'], ['jekyll-build']);
-  gulp.watch('sass/*.scss', ['pre-process', 'minify-css']);
-  gulp.watch('css/nkd.css', ['bs-reload', 'minify-css']);
+gulp.task('default', ['pre-process', 'minify-css', 'bs-reload', 'browser-sync', 'shorthand'], function(){
+  gulp.start('pre-process', 'csslint', 'shorthand');
+  gulp.watch(['_includes/*', '_layouts/*', '_posts/*', '_resources/*', '_sass/*', '*.html', '_*config.yml']);
+  gulp.watch('sass/*.scss', ['pre-process']);
   gulp.watch('*.html', ['bs-reload'])
 });
